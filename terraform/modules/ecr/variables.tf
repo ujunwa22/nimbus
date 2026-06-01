5ed4f84c612fc8856fd51637e2264fc18e6573e6
Obianuju Ogaziechi <obianuju.ogaziechi22@gmail.com>
2026-06-01 19:09:11 +0100

    fix: correct ECR module variables

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,17 +1,12 @@
-variable "nimbus_project" {
-    description = "Project name prefix for ECR repositories"
+variable "project" {
     type = string
-    default = "nimbus_ecr"
 }
 
-variable "nimbus_env" {
-    description = "Environment name for ECR repositories"
+variable "env" {
     type = string
-    default = "dev"
 }
 
 variable "tags" {
-  description = "Tags for ECR repositories"
   type    = map(string)
   default = {}
 }
