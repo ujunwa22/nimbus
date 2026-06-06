e32ad2b8ca231ced0d85b6556e61ae9973583da1
ujunwa22 <101198154+ujunwa22@users.noreply.github.com>
2026-06-06 21:31:53 +0100

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -209,12 +209,10 @@ output "github_actions_role_arn" {
 
 output "db_password_arn" {
   value = module.secrets.db_password_arn
-  sensitive =true
 }
 
 output "jwt_secret_arn" {
   value = module.secrets.jwt_secret_arn
-  sensitive = true
 }
 
 output "alb_dns_name" {
